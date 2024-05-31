51b275e7699c967071639588d8e6ff369f54da30
miwik <rem.bourreau@gmail.com>
2024-05-31 15:27:48 +0200

    Save & restore morrowind dir + start and launch morrowind + run navmesh

--- morrowind.py
+++ morrowind.py
@@ -3,6 +3,7 @@ import logging
 import subprocess
 import os
 import json
+import shutil
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -18,6 +19,33 @@ def load_file_as_json(json_file):
 
 # ---------------------------------------------------------------------------------------------------------------------
 
+def remove_directory(directory_path):
+    if os.path.exists(directory_path) and os.path.isdir(directory_path):
+        try:
+            shutil.rmtree(directory_path)
+            logging.info("Directory %s and all its contents have been removed.", directory_path)
+        except Exception as e:
+            logging.error("An error occurred while removing the directory: %s", e)
+    else:
+        logging.warning("The directory %s does not exist or is not a directory.", directory_path)
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def copy_directory(src, dst):
+    if os.path.isdir(dst):
+        remove_directory(dst)
+    try:
+        shutil.copytree(src, dst)
+        logging.info("Directory %s successfully copied to %s", src, dst)
+        return True
+    except shutil.Error as e:
+        logging.error("Error occurred while copying directory: %s", e)
+    except OSError as e:
+        logging.error("OS error occurred while copying directory: %s", e)
+    return False
+
+# ---------------------------------------------------------------------------------------------------------------------
+
 def load_config(configfile):
     script_path = os.path.dirname(__file__)
     config = load_file_as_json(os.path.join(script_path, configfile))
@@ -27,13 +55,33 @@ def load_config(configfile):
 
 # ---------------------------------------------------------------------------------------------------------------------
 
-def start_morrowind(config):
+def start(config):
     logging.info("Starting Morrowind...")
     subprocess.Popen([os.path.join(config["openmw_dir"], "openmw")])
     
 # ---------------------------------------------------------------------------------------------------------------------
 
+def launcher(config):
+    logging.info("Running the launcher...")
+    subprocess.Popen([os.path.join(config["openmw_dir"], "openmw-launcher")])
 
+# ---------------------------------------------------------------------------------------------------------------------
+
+def navmeshtool(config):
+    logging.info("Running navmeshtool...")
+    subprocess.Popen([os.path.join(config["openmw_dir"], "openmw-navmeshtool")])
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def save(config, name):
+    logging.info("Saving game directory to %s...", name)
+    return copy_directory(config["morrowind_dir"], os.path.join(config["steam_common_dir"], config["morrowind_dir_name"] + " (" + name + ")"))
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def restore(config, name):
+    logging.info("Restoring game directory %s...", name)
+    return copy_directory(os.path.join(config["steam_common_dir"], config["morrowind_dir_name"] + " (" + name + ")"), config["morrowind_dir"])
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -54,20 +102,28 @@ def main():
     launcher_parser = subparsers.add_parser("launcher", help="Run the launcher.")
 
     # Command: navmeshtool
-    navmeshtool_parser = subparsers.add_parser("navmeshtool", help="Run navmeshtool.")
+    navmeshtool_parser = subparsers.add_parser("navmeshtool", help="Run navmeshtool to generate navmeshs.")
+
+    # Command: save
+    save_parser = subparsers.add_parser("save", help="Save game directory.")
+    save_parser.add_argument("name", help="Name of the save.")
+
+    # Command: restore
+    restore_parser = subparsers.add_parser("restore", help="Restore game directory.")
+    restore_parser.add_argument("name", help="Name of the save.")
 
     # Commands execution
     args = parser.parse_args()
     if args.command == "start" or args.command is None:
-        start_morrowind(config)
+        start(config)
     elif args.command == "launcher":
-        pass
+        launcher(config)
     elif args.command == "navmeshtool":
-        pass
+        navmeshtool(config)
     elif args.command == "save":
-        pass
+        save(config, args.name)
     elif args.command == "restore":
-        pass
+        restore(config, args.name)
     else:
         parser.print_help()
 
